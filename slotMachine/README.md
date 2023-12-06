a32b4b82cb5f81d185b4b4aeba54be337a048f3f
Derek <derekbl@uci.edu>
2023-12-05 16:20:15 -0800

    updating

---
+++ slotMachine/README.md
@@ -0,0 +1,7 @@
+PROJECT: SLOT MACHINE
+
+Step 1: PROJECT INITIALIZATION
+
+1. Create a javascript file
+2. Run: npm init
+3. Run: npm i prompt-sync (gets user input)
---
+++ slotMachine/script.js
@@ -0,0 +1,13 @@
+// 1) Put some money in the slot machine (deposit)
+
+// 2) Determine the number of lines to bet on (3 rows/lines you can bet on at MAX)
+
+// 2.1) How much the user is betting on slot machine (colect bet)
+
+// 3) Spin the slot machine
+
+// 4) Check if the user won
+
+// 5) Give the user their winnings
+
+// 6) Play again
